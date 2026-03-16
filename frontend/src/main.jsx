5187046a42a83e44d47be2211a46d9fd4671f58e
Karan Sahani <mohansahani.karan@gmail.com>
2026-03-16 20:15:13 +0530

    lowercase sensitivity

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import About from "./About.jsx";
 import "./index.css";
-import ContactPage from "./Contact.jsx";
-import Demo from "./Demo.jsx";
+
+
+import About from "./about.jsx";
+import ContactPage from "./contact.jsx";
+import Demo from "./demo.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/"      element={<App />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/"        element={<App />} />
+        <Route path="/about"   element={<About />} />
         <Route path="/contact" element={<ContactPage />} />
-        <Route path="/demo" element={<Demo />} />
+        <Route path="/demo"    element={<Demo />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
